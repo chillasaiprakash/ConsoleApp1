efa57bb68cabf5f09dfa849fc007308d9ab0fdec
ch saiprakash <chillasaiprakash@outlook.com>
2021-06-06 10:43:10 +0530

    updated

---
+++ p8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p8
+{
+    class Program
+    {
+        //How to remove duplicate characters from a string
+        static void Main(string[] args)
+        {
+ 
+            Console.Write("Enter a String : ");
+            string duplicate = Console.ReadLine();
+            string unique = "";
+            for (int i = 0; i < duplicate.Length; i++)
+            {
+                if (!unique.Contains(duplicate[i]))
+                {
+                    unique += duplicate[i];
+                }
+            }
+            Console.WriteLine(unique);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ p9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p9
+{
+    //How to find all possible substring of a given string
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a String : ");
+            string String = Console.ReadLine();
+
+            Console.WriteLine("All substrings for given string are : ");
+
+            for (int i = 0; i < String.Length; ++i)
+            {
+                StringBuilder subString = new StringBuilder(String.Length - i);
+                for (int j = i; j < String.Length; ++j)
+                {
+                    subString.Append(String[j]);
+                    Console.Write(subString + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
